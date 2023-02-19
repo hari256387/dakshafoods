7ba9d6ca9a3e1836c72a8244c5138c31791e1a16
hari256387 <108853643+hari256387@users.noreply.github.com>
2023-02-19 21:12:42 +0530

    Add files via upload

---
+++ add-admin.php
@@ -0,0 +1,98 @@
+<?php include('partials/menu.php'); ?>
+
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+
+        <br><br>
+
+        <?php
+            if(isset($_SESSION['add'])) //checking wheteher the session
+            {
+              echo $_SESSION['add']; //display the session message if set
+              unset($_SESSION['add']);  //Remove session message
+            }
+        ?>
+
+        <form action="" method="POST">
+
+            <Table class="tbl-30">
+                <tr>
+                    <td>Full Name</td>
+                    <td>
+                        <input type="text" name="full_name" placeholder="Enter your name">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <input type="text" name="username" placeholder="your username">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td>
+                        <input type="password" name="password" placeholder="Your password">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
+
+                    </td>
+                </tr>
+            </Table>
+        </form>
+    </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
+
+<?php
+    //process the value from form and save it in database
+    //check whether the submit button is clicked or not
+
+    if(isset($_POST['submit']))
+    {
+        // button clicked
+        //echo "Button clicked";
+        //1.get the data from form
+       $full_name= $_POST['full_name'];
+       $username = $_POST['username'];
+       $password = md5($_POST['password']); //password encryption with md5
+       //2.SQL Query to save the data into database
+       $sql = "INSERT INTO tbl_admin SET
+            full_name='$full_name',
+            username='$username',
+            password='$password'
+       ";
+       
+       //3. excuting query and saving data into database
+
+        $res = mysqli_query($conn, $sql) or die(mysqli_error());
+
+        //4. check whether the query is executed data is inserted or not and display appropriate message 
+        if($res==TRUE)
+        {
+
+            //data inserted
+           // echo "data inserted";
+           //create a session variable to display message
+           $_SESSION['add'] = "Admin added successfully";
+           //redirect page
+           header("location:".SITEURL.'admin/manage-admin.php');
+        }
+        else
+        {
+            //failed to insert data
+            //echo "fails to insert data";
+             //create a session variable to display message
+           $_SESSION['add'] = "fail to add admin";
+           //redirect page
+           header("location:".SITEURL.'admin/add-admin.php');
+        
+        }
+    }
+    
+?>
---
+++ manage-admin.php
@@ -0,0 +1,85 @@
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main content section starts --->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+
+                <br/>
+
+                <?php
+                    if(isset($_SESSION['add']))
+                    {
+                        echo $_SESSION['add']; //displaying session message
+                        unset($_SESSION['add']); //receiving session message
+                    }
+                ?>
+                <br><br><br>
+
+                <!-- Button to add admin -->
+                <a href="add-admin.php" class="btn-primary">Add Admin</a>
+                <br/><br/><br/>
+                
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <?php
+                        //Query to get all admin
+                        $sql = "SELECT * FROM tbl_admin";
+                        //Execute the query
+                        $res = mysqli_query($conn, $sql);
+
+                        //check whether the query is executed of not
+                        if($res==TRUE)
+                        {
+                            //count rows to check whether data in database 
+                            $count = mysqli_num_rows($res); // function
+
+                            $sn=1; //create a variable and assign the value
+
+                            //check the num of rows
+                            if($count>0)
+                            {
+                                //we have data in database
+                                while($rows=mysqli_fetch_assoc($res))
+                                {
+                                    //using while to get all the data from database
+                                    //and while loop will run as long as data in database
+
+                                    //get individaul data
+                                    $id=$rows['id'];
+                                    $full_name=$rows['full_name'];
+                                    $username=$rows['username'];
+
+                                    //display the values in our table
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $sn++; ?> </td>
+                                            <td><?php echo $full_name; ?></td>
+                                            <td><?php echo $username; ?></td>
+                                            <td>
+                                                <a href="#" class="btn-secondary">Update Admin</a>
+                                                <a href="#" class="btn-danger">Delete Admin</a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                }
+                            }
+                            else
+                            {
+                                //we do not have data in database
+                            }
+                        }
+                    ?>
+
+                </table>
+            </div>
+        </div>
+        <!-- Menu content section ends --->
+
+<?php include('partials/footer.php'); ?>
