aaa7f7a592befdb575f7dd82619a07b63faa5201
hari256387 <108853643+hari256387@users.noreply.github.com>
2023-02-19 21:13:46 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+ <!-- Footer section starts --->
+ <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Chittoor kitchen. Developed by - <a href="#"> hari</a></p>
+            </div>
+        </div>
+        <!-- Footer section starts --->
+
+    </body>
+    
+</html>
